6d17591a6d8d566d31a208a454619e0bc6678495
ehgus6256 <ehgus6256@naver.com>
2022-11-01 10:08:52 +0900

    2022-11-01 bit python basic

---
+++ Q1.py
@@ -0,0 +1,27 @@
+a = [2,3,4,5,10]
+b = [4,5,6,7,8]
+set_a = set(a)
+set_b = set(b)
+x = list(set_a & set_b)
+x.sort()
+print(x[len(x)-1])
+
+
+
+or
+
+
+
+a, b = [2,3,4,5,10], [4,5,6,7,8]
+set_a, set_b = set(a), set(b)
+x = list(set_a & set_b)
+x.sort()
+c.reverse()
+print(x[0])
+
+
+
+
+a와b의 교집합중 최댓값 출력하는방법
+
+
