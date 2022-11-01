f19146eb1fff8ec6c288426d0b4ec5c6fe205f70
ehgus6256 <ehgus6256@naver.com>
2022-11-01 11:45:50 +0900

    #4 update

--- Q2.py
+++ Q2.py
@@ -6,3 +6,19 @@ print("hi")
 
 # 두번째
 # 세번째
+
+# 조건문
+# 5는 짝수 True False x
+# 5는 짝수입니다
+# 5는 짝수 아닙니다
+
+num = 6
+if num % 3 == 1 : 
+    print("5는 짝수입니다")
+elif num % 3 == 2:
+    print("5는 짝수가 아닙니다")
+else:
+    print("else")
+    print("다")
+    print("아님")
+#!= 아닐때
