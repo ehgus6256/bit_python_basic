651a38975dd65697bc8e5ab6f7f65495521137b5
ehgus6256 <ehgus6256@naver.com>
2022-11-07 17:31:47 +0900

    update

--- kakao/kakao_talk.py
+++ kakao/kakao_talk.py
@@ -1,6 +1,6 @@
 from kakao.kakao_id import kakao_id
 
-class kakao_pay(kakao_id):
+class kakao_talk(kakao_id):
     친구 = []
     def __init__(self, _id, _password, _name, _친구 ) -> None:
         super().__init__(_id, _password, _name, _친구)
--- kakao/kakao_taxi.py
+++ kakao/kakao_taxi.py
@@ -1,6 +1,6 @@
 from kakao.kakao_id import kakao_id
 
-class kakao_pay(kakao_id):
+class kakao_taxi(kakao_id):
     내위치 = ""
     목적지 = "" 
     def __init__(self, _id, _password, _name, _내위치, _목적지 ) -> None:
